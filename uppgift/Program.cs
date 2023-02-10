f829f0286a668a438b74b57caaa58fb453ae5e3b
Acrobaticbag3 <111962656+Dungeater1337@users.noreply.github.com>
2023-02-10 10:11:57 +0100

    Add project files.

---
+++ uppgift/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace upggift6_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            int summa= förstatalet(5,120,50);
+            Console.WriteLine(summa);   
+
+        }
+        static int förstatalet (int tal1, int tal2, int tal3)
+        {
+            return tal1 + tal2 + tal3;
+        }
+    }
+}
